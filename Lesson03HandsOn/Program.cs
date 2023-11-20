24b1e921c4ac531cc5f9ef6c34cc77589e151026
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2023-11-20 15:51:59 -0500

    Lesson03HandsOn
    Created a simple dotnet project and then set up an array names, then ran a for loop that ran through the array and printed the phrase "Have you seen ____?" with the names from the array filling in the blanks, then ran the same array through another loop that did the same thing but in reverse order.

---
+++ Lesson03HandsOn/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson03HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameArray = {"Emily", "Harry", "Rupert", "Clara", "Lily", "Michael"};
+
+            for(int i = 0; i < nameArray.Length; i++)
+            {
+                Console.WriteLine("Have you seen " + nameArray[i] + "?");
+            }
+
+            for(int i = nameArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine("Have you seen " + nameArray[i] + "?");
+            }
+        }
+    }
+}
